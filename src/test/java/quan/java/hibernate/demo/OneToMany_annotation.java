cf98f7f168671d5eb88832763d390ad6cd713a90
liuquan <liuquan@liuquandeMacBook-Pro.local>
2017-11-05 21:36:41 +0800

    单元测试

--- src/test/java/quan/java/hibernate/demo/OneToMany_annotation.java
+++
@@ -1,7 +0,0 @@
-package quan.java.hibernate.demo;
-
-/**
- * Created by liuquan on 2017/11/5.
- */
-public class OneToMany_annotation {
-}
---
+++ src/test/java/quan/java/hibernate/demo/OneToMany_annotationTest.java
@@ -0,0 +1,19 @@
+package quan.java.hibernate.demo;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+
+public class OneToMany_annotationTest {
+
+    @Test
+    public void hello() {
+        assertEquals("hello","hello");
+    }
+
+    @Test
+    public void isNull() {
+        assertNull("");
+    }
+}
